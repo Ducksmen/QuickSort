0cebeaf718ace7de82c8a37e01d2ce65ad0aeaea
ducksmen <jhoang6606@bths.edu>
2018-11-29 17:57:59 -0500

    a

--- src/QuickSort.java
+++ src/QuickSort.java
@@ -2,46 +2,33 @@ import java.util.Random;
 
 public class QuickSort
 {
-    public void quickSort(int[] arr, int l, int r)
+    public static void quicksort(int[] arr, int l, int r)
     {
-        if (l<r)
+        if(l<r)
         {
             int pivot = partition(arr, l, r);
-
-            quickSort(arr, l,pivot-1);
-            quickSort(arr,pivot+1, r);
+            quicksort(arr, l,pivot-1);
+            quicksort(arr,pivot+1, r);
         }
     }
 
-    public int partition(int[] arr, int l, int r)
+    public static int partition(int[] arr, int l, int r)
     {
-        int temp = 0;
         int pivot = arr[r];
-        int i = l -1;
-        for(int j=l;j<=r;j++)
+        int i = l-1;
+        for(int j=l;j<r;j++)
         {
-            if(j<=pivot)
+            if(arr[j]<=pivot)
             {
                 i++;
-                temp=arr[i];
-                arr[i]=arr[j];
-                arr[j]=temp;
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
             }
         }
-        temp=arr[i];
-        arr[i]=arr[r];
-        arr[r]=temp;
+        int temp = arr[i+1];
+        arr[i+1] = arr[r];
+        arr[r] = temp;
         return i+1;
-    }
-
-    public int[] genInts(int n)
-    {
-        int[] list1 = new int[n];
-        Random random = new Random();
-        for(int i = 0; i < n; i++)
-        {
-            list1[i] = random.nextInt(10000);
-        }
-        return list1;
     }
 }
--- src/Runner.java
+++ src/Runner.java
@@ -5,10 +5,9 @@ public class Runner
 {
     public static void main(String[] args)
     {
-        QuickSort list1 = new QuickSort();
-        int[] intArr = list1.genInts(10);
+        int[] intArr = {1,5,2,7,4,6,9,3};
         System.out.println(Arrays.toString(intArr));
-        list1.quickSort(intArr,0,9);
+        QuickSort.quicksort(intArr,0,intArr.length-1);
         System.out.println(Arrays.toString(intArr));
     }
 }
